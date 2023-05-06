9a28c9dfe02f294206cb5532472f5c7a8c1fc9a9
Yaswanth Reddy <100778355+Yaswanth235@users.noreply.github.com>
2023-05-06 11:46:14 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Reverse a sublist of a linked list/reverse-a-sublist-of-a-linked-list.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*Link list node 
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};*/
+
+class Solution{
+    public:
+    Node* reverseBetween(Node* head,int m,int n){
+          Node* temp=head;
+       vector<int>v;
+       while(temp!=NULL){
+           v.push_back(temp->data);
+           temp=temp->next;
+       }
+       reverse(v.begin()+m-1,v.begin()+n);
+       temp=head;
+       int i=0;
+       while(temp!=NULL){
+           temp->data=v[i++];
+           temp=temp->next;
+       }
+       return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Function to print linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
+
+
+
+// Driver program to test above functions
+int main()
+{
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int N, m, n;
+		cin >> N>>m>>n;
+
+		Node *head = NULL;
+		Node *temp = head;
+
+		for (int i = 0; i < N; i++) {
+			int data;
+			cin >> data;
+			if (head == NULL)
+				head = temp = new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+
+		
+
+        Solution ob;
+
+		Node* newhead = ob.reverseBetween(head, m, n);
+		printList(newhead);
+
+		cout << "\n";
+
+
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends
