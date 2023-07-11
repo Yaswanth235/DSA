4b142e5c85e6b3337622a54aba5099a0fb056590
Yaswanth Reddy <100778355+Yaswanth235@users.noreply.github.com>
2023-07-11 07:50:48 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find kth element of spiral matrix/find-kth-element-of-spiral-matrix.cpp
+++ Medium/Find kth element of spiral matrix/find-kth-element-of-spiral-matrix.cpp
@@ -7,31 +7,32 @@ using namespace std;
 class Solution{
     public:
     int findK(int a[MAX][MAX],int n,int m,int k){
- 		int top = 0; 
-int bottom=n-1; 
-int left=0; 
-int right=m-1; 
-vector<int>ans; 
-while(top<=bottom&&left<=right){ 
-    for(int i=left;i<=right;i++) 
+ 		int top=0; 
+        int bottom=n-1; 
+        int left=0; 
+        int right=m-1; 
+        vector<int>ans; 
+   while(top<=bottom&&left<=right){ 
+    for(int i=left;i<=right;i++){
        ans.push_back(a[top][i]); 
-       top++; 
- 
-    for(int i=top;i<=bottom;i++) 
-       ans.push_back(a[i][right]); 
-       right--;
-     
-      if(top<=bottom){ 
-    for(int i=right;i>=left;i--) 
-       ans.push_back(a[bottom][i]); 
-       bottom--; 
     }
-    if(left<=right){ 
-        for(int i=bottom;i>=top;i--) 
+    top++;
+ 
+    for(int i=top;i<=bottom;i++){
+       ans.push_back(a[i][right]);
+    }
+    right--;
+     
+    for(int i=right;i>=left;i--){
+       ans.push_back(a[bottom][i]); 
+    }
+    bottom--;
+    
+        for(int i=bottom;i>=top;i--){
         ans.push_back(a[i][left]); 
-        left++; 
-        } 
-      } 
+      }
+      left++;
+   }
     return ans[k-1];
     }
 };
